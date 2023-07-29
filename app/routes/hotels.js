c270ed75b3c266f8ec3fcebfc8347760fccb270e
LeonelAlegreDev <leonelalegre.dev@gmail.com>
2023-07-29 20:55:13 -0300

    Se desinstalo la libreria express-expeditiuos y expeditious-engine-redis por presentar vulnerabilidad de seguridad.

--- app/routes/hotels.js
+++ app/routes/hotels.js
@@ -1,20 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const checkOrigin = require('../middleware/origin')
-const { cacheInit } = require('../middleware/cache')
 const { getItems, getItem, createItem, deleteItem, updateItem } = require('../controlles/hotels')
 
 //TODO: Turbo 🐱‍🏍  cache!
 router.get(
     '/',
     checkOrigin,
-    cacheInit, //TODO: <--- 😨 ¿WTF?
     getItems
 )
 
 router.get('/:id', checkOrigin, getItem)
 
-router.post('/', checkOrigin, cacheInit, createItem)
+router.post('/', checkOrigin, createItem)
 
 router.patch('/:id', updateItem)
 
